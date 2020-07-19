9b6dcc433ff0da2a29e15444e34b1516868ad4a2
ricardomarzochi@gmail.com <r.libera.marzochi@CPX-JGDMHHPGOXX.dir.svc.accenture.com>
2020-07-19 19:30:14 +0200

    NewVersion/0.2.0

--- src/main/java/com/packsendme/microservice/manager/roadway/repository/IRoadwayManager_Repository.java
+++ src/main/java/com/packsendme/microservice/manager/roadway/repository/IRoadwayManager_Repository.java
@@ -2,7 +2,6 @@ package com.packsendme.microservice.manager.roadway.repository;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.data.mongodb.repository.Query;
-import org.springframework.stereotype.Repository;
 
 public interface IRoadwayManager_Repository<T> extends MongoRepository<T, String> {
 	
@@ -13,5 +12,5 @@ public interface IRoadwayManager_Repository<T> extends MongoRepository<T, String
 	BodyWork_Model findBodyworkByName(String bodyWork);
 	
 	@Query("{'category_name' :  {$eq: ?0}}")
-	RoadwayBusinessRule_Model findRoadwayByCategory(String category_name);
+	T findRoadwayByCategory(String category_name);
 }
