0c782ec8c5d2785564b9c5aaa400ada8b45915f9
ricardomarzochi@gmail.com <r.libera.marzochi@CPX-JGDMHHPGOXX.dir.svc.accenture.com>
2020-07-19 18:05:08 +0200

    NewVersion/0.2.0

--- src/main/java/com/packsendme/microservice/manager/roadway/repository/IRoadwayManager_Repository.java
+++ src/main/java/com/packsendme/microservice/manager/roadway/repository/IRoadwayManager_Repository.java
@@ -4,7 +4,6 @@ import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
 
-@Repository
 public interface IRoadwayManager_Repository<T> extends MongoRepository<T, String> {
 	
 	@Query("{'vehicle' :  {$eq: ?0}}")
