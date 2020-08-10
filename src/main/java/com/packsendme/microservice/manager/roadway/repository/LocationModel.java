33e110997abf08c613a1e1ada5dc863991db3e7b
ricardomarzochi@gmail.com <r.libera.marzochi@CPX-JGDMHHPGOXX.dir.svc.accenture.com>
2020-08-10 18:47:39 +0200

    NewVersion/Structus

---
+++ src/main/java/com/packsendme/microservice/manager/roadway/repository/LocationModel.java
@@ -0,0 +1,41 @@
+package com.packsendme.microservice.manager.roadway.repository;
+
+import java.io.Serializable;
+
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter 
+@Setter
+@Document(collection = "LocationManager_SA")
+public class LocationModel implements Serializable{
+	
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	public String countryName;
+	public String cityName;
+	public String stateName;
+	public String codCountry;
+	
+	
+	public LocationModel(String countryName, String cityName, String stateName, String codCountry) {
+		super();
+		this.countryName = countryName;
+		this.cityName = cityName;
+		this.stateName = stateName;
+		this.codCountry = codCountry;
+	}
+
+
+	public LocationModel() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+	
+	
+
+}
