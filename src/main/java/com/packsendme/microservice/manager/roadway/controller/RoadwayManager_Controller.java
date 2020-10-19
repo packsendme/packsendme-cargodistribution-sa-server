36fd54088a44e1724fdc83de8a4da4ddffa08492
ricardomarzochi@gmail.com <r.libera.marzochi@CPX-JGDMHHPGOXX.dir.svc.accenture.com>
2020-10-19 19:33:14 +0200

    add

--- src/main/java/com/packsendme/microservice/manager/roadway/controller/RoadwayManager_Controller.java
+++ src/main/java/com/packsendme/microservice/manager/roadway/controller/RoadwayManager_Controller.java
@@ -6,6 +6,7 @@ import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestAttribute;
@@ -141,7 +142,7 @@ public class RoadwayManager_Controller {
 	@GetMapping("/category/transport/{transport}")
 	public ResponseEntity<?> getCategory(@RequestHeader("isoLanguageCode") String isoLanguageCode,@RequestHeader("isoCountryCode") String isoCountryCode,
 			@RequestHeader("isoCurrencyCode") String isoCurrencyCode,@RequestHeader("originApp") String originApp,
-			@Validated  @RequestAttribute String transport) {	
+			@Validated  @PathVariable("transport") String transport) {	
 		return categoryService.findCategoriesByTransports(transport);
 	}
 
